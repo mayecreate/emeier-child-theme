2632f01917a81edca8a6552fb3af2128ffae9471
gbg-webops <106274826+gbg-webops@users.noreply.github.com>
2023-04-13 15:59:59 -0500

    Updating header.php to add the title back in

--- header.php
+++ header.php
@@ -5,7 +5,35 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
 
+<?php 
 
+/**
+ * ==========================================================
+ * MayeCreate Title
+ * ==========================================================
+ */
+
+	echo '<title>';
+	
+	/* Print the <title> tag based on what is being viewed. */
+	global $page, $paged;
+	
+	wp_title( '|', true, 'right' );
+
+	// Add the blog name.
+	bloginfo( 'name' );
+
+	// Add the blog description for the home/front page.
+	$site_description = get_bloginfo( 'description', 'display' );
+	if ( $site_description && ( is_home() || is_front_page() ) )
+		echo " | $site_description";
+
+	// Add a page number if necessary:
+	if ( $paged >= 2 || $page >= 2 )
+		echo ' | ' . sprintf( __( 'Page %s', 'skematik' ), max( $paged, $page ) );
+	
+	echo '</title>';
+?>
 
 
 
